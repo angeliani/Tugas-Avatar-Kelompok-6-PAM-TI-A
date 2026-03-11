6afe390224fb5f52dd07ecea7f5a4a9bb3f383e0
angel <angeliani.junitaviana@gmail.com>
2026-03-11 15:05:26 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/kelompok_6/MainActivity.kt
+++ app/src/main/java/com/example/kelompok_6/MainActivity.kt
@@ -5,25 +5,42 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.kelompok_6.ui.theme.Kelompok_6Theme
+import androidx.compose.ui.graphics.Color
+
+// warna
+val PinkBackground = Color(0xFFFDF1F4)
+val PinkPrimary = Color(0xFFE91E63)
+val PinkLight = Color(0xFFF8BBD0)
+val PinkButton = Color(0xFFFF4081)
+val White = Color.White
+val TextDark = Color.DarkGray
+
+// data class user
+data class UserData(
+    val namaDepan: String,
+    val namaBelakang: String,
+    val namaPanggilan: String,
+    val email: String,
+    val password: String,
+    val noHp: String,
+    val alamat: String
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Kelompok_6Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = PinkBackground
+                ) {
+                    AppNavigator()
                 }
             }
         }
@@ -31,17 +48,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AppNavigator() {
+    var halaman by remember { mutableStateOf("register") }
+    var userData by remember { mutableStateOf<UserData?>(null) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Kelompok_6Theme {
-        Greeting("Android")
+    when (halaman) {
+        "register" -> HalamanRegister(
+            keLogin = { halaman = "login" },
+            onRegisterSuccess = {
+                userData = it
+                halaman = "login"
+            }
+        )
+        "login" -> HalamanLogin(
+            keRegister = { halaman = "register" },
+            onLoginSuccess = { halaman = "avatar" }
+        )
+        "avatar" -> AvatarScreen(
+            onNext = { halaman = "profil" }
+        )
+        "profil" -> {
+            val dataTampil = userData ?: UserData(
+                "Angeliani", "Junitaviana", "Angel", "angeliani@student.ub.ac.id", "123", "081234567890", "Malang"
+            )
+            HalamanProfilUser(
+                userData = dataTampil,
+                onBackClick = { halaman = "avatar" },
+                onLogoutClick = { halaman = "login" }
+            )
+        }
     }
 }
